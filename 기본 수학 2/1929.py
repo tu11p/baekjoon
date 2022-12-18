cf421eaaa82614ffcd3ad971815ac5e5132b495b
tu11p <jamsilkes@kyonggi.ac.kr>
2022-12-18 15:23:31 +0900

    기본 수학 2

---
+++ 기본 수학 2/1929.py
@@ -0,0 +1,13 @@
+# 에라토스테네스의 체
+import sys
+
+min, max = map(int,sys.stdin.readline().split())
+
+for i in range(min, max + 1):
+    if i == 1:
+        continue
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
