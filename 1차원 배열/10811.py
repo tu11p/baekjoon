52bdba73c1b1145606f391ef49890d51dab7536d
tu11p <jamsilkes@kyonggi.ac.kr>
2023-03-15 15:54:23 +0900

    1차원 배열

---
+++ 1차원 배열/10811.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+order = [i for i in range(1, n + 1)]
+for a in range(m):
+    i, j = map(int, input().split())
+    r = j - i + 1
+    for _ in range(r // 2):
+        temp = order[i - 1]
+        order[i - 1] = order[j - 1]
+        order[j - 1] = temp
+        i += 1
+        j -= 1
+
+for b in order:
+    print(b, end=' ')
